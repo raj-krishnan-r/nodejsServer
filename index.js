a71148dde050afada5ee133f81dcfd5dc13b9791
Raj Krishnan R <rkrishnan1993@live.com>
2015-12-21 23:08:08 +0530

    init at this

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var port = 3709;
+app.set('views',__dirname+'/tpl');
+app.set('view engine',"jade");
+app.engine('jade',require('jade').__express);
+app.get("/",function(req,res){
+//res.send("IT is Up");
+res.render("page");
+});
+app.use(express.static(__dirname+'/public'));
+//app.listen(port);
+var io=require('socket.io').listen(app.listen(port));
+io.sockets.on('connection',function(socket)
+{
+socket.emit('message',{message:'welcome to Notes Chat'});
+socket.on('send',function(data){
+io.sockets.emit('message',data);
+});
+});
+
+console.log('http://127.0.0.1:'+port);
