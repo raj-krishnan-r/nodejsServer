3dae26971dfcd1dc39697b107b075c74fc5b0239
Raj Krishnan R <rkrishnan1993@live.com>
2015-12-23 08:09:07 +0530

    The server now recieves any file request and forwards it to client, bingo onwat

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.get('/',function(req,res)
+var url = require('url');
+var fs = require('fs');
+//console.log(url.parse('http://www.google.com'));
+app.get('/*',function(req,res)
 {
-res.sendFile(__dirname+'/index.html');
+//res.sendFile(__dirname+'/index.html');
+var forwardFile = __dirname+url.parse(req.url).path;
+console.log(forwardFile);
+fs.stat(forwardFile, function(err, stats) {
+            if (err) {
+             //   throw err;
+                console.log(err);
+                res.sendFile(__dirname+'/index.html');
+
+            }
+            else{
+res.sendFile(__dirname+url.parse(req.url).path);
+            }
+            });
 });
 
 io.on('connection',function(socket){
 console.log('A user Connected');
 socket.on('disconnect',function(){
 console.log('One User Disconnected');
+socket.on('chat message',function(msg)
+{
+io.emit('disconnected','dis');
+});
 });
 socket.on('chat message',function(msg)
 {
@@ -24,7 +44,7 @@ io.emit('click num',msg);
 });
 });
 
-http.listen(3001,function()
+http.listen(3000,function()
 {
-console.log('listening on *:3001');
+console.log('listening on *:3000');
 });
